a3b0899e919dae5bc16777a63dc1823947fd5686
batTrung <trungbat.pv@gmail.com>
2020-07-10 11:52:32 +0700

    add flake8, isort setup

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,8 @@ SECRET_KEY = config('SECRET_KEY', default=string.ascii_letters)
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv(),
+)
 
 
 DJANGO_APPS = [
