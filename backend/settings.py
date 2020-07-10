fd34debe6797fbca9cb4f49559d273cd29eb41a5
batTrung <trungbat.pv@gmail.com>
2020-07-10 11:45:27 +0700

    Initial django project

---
+++ backend/settings.py
@@ -0,0 +1,153 @@
+import os
+import string
+
+import dj_database_url
+from decouple import Csv, config
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# ==============================================================================
+# CORE SETTINGS
+# ==============================================================================
+
+SECRET_KEY = config('SECRET_KEY', default=string.ascii_letters)
+
+DEBUG = config('DEBUG', default=True, cast=bool)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+
+
+DJANGO_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+THIRD_PARTY_APPS = [
+
+]
+
+LOCAL_APPS = [
+    'apps.common.apps.CommonConfig',
+    'apps.core.apps.CoreConfig',
+    'apps.accounts.apps.AccountsConfig',
+    'apps.blog.apps.BlogConfig',
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+SITE_ID = 1
+
+INTERNAL_IPS = ['127.0.0.1']
+
+if config("USE_DOCKER", default=False, cast=bool):
+    import socket
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())  # noqa
+    INTERNAL_IPS += [ip[:-1] + "1" for ip in ips]
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default='sqlite:///%s' % os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
+}
+
+ROOT_URLCONF = 'backend.urls'
+
+WSGI_APPLICATION = 'backend.wsgi.application'
+
+ADMIN_URL = config('ADMIN_URL', default='admin/')
+
+
+# ==============================================================================
+# MIDDLEWARE SETTINGS
+# ==============================================================================
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+# ==============================================================================
+# TEMPLATES SETTINGS
+# ==============================================================================
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+# ==============================================================================
+# AUTHENTICATION AND AUTHORIZATION SETTINGS
+# ==============================================================================
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+# ==============================================================================
+# INTERNATIONALIZATION AND LOCALIZATION SETTINGS
+# ==============================================================================
+
+LANGUAGE_CODE = config('LANGUAGE_CODE', default='en')
+
+TIME_ZONE = config('TIME_ZONE', default='UTC')
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# ==============================================================================
+# STATIC & MEDIA FILES SETTINGS
+# ==============================================================================
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+
+]
+
+# ==============================================================================
+# EMAIL SETTINGS
+# ==============================================================================
