14cd86b160a450a1c1bc8b73911feca0097c1a38
batTrung <trungbat.pv@gmail.com>
2020-07-11 19:49:23 +0700

    get name instead of id website

--- apps/blog/api/serializers.py
+++ apps/blog/api/serializers.py
@@ -4,10 +4,13 @@ from ..models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    website = serializers.ReadOnlyField(source='website.get_name')
+
     class Meta:
         model = Post
         fields = (
             'website',
+            'title',
             'link',
             'views',
             'created',
