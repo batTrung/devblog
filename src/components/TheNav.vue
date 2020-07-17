fc083fd58f882e5f785887c0ca123c79b4976f63
batTrung <trungbat.pv@gmail.com>
2020-07-17 22:23:21 +0700

    add nav icons

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -6,26 +6,38 @@
                 <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
                 <b-collapse id="nav-collapse" is-nav>
                     <b-navbar-nav>
-                        <b-nav-item :to="{name: 'home'}">Books</b-nav-item>
-                        <b-nav-item :to="{name: 'home'}">About</b-nav-item>
-                        <b-nav-item :to="{name: 'home'}">Contact</b-nav-item>
+                        <b-nav-item :to="{name: 'home'}">Trang</b-nav-item>
                     </b-navbar-nav>
                     <b-navbar-nav class="ml-auto">
+                        <b-nav-form>
+                            <b-form-input class="mr-sm-2" placeholder="Search"></b-form-input>
+                            <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Search</b-button>
+                        </b-nav-form>
+                        <b-nav-item-dropdown text="trungbat" right>
+                            <b-dropdown-item>Trang cá nhân</b-dropdown-item>
+                            <b-dropdown-item>Cài đặt</b-dropdown-item>
+                            <div class="dropdown-divider"></div>
+                            <b-dropdown-item>Đăng xuất</b-dropdown-item>
+                        </b-nav-item-dropdown>
                     </b-navbar-nav>
                 </b-collapse>
-                <b-nav-form>
-      <b-form-input class="mr-sm-2" placeholder="Search"></b-form-input>
-      <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Search</b-button>
-    </b-nav-form>
             </div>
         </b-navbar>
         <div class="nav-scroller bg-white box-shadow">
             <div class="container">
                 <nav class="nav nav-underline">
-                    <a class="nav-link active" href="#"><b-badge pill variant="primary">Primary</b-badge></a>
-                    <a class="nav-link active" href="#"><b-badge pill variant="primary">Primary</b-badge></a>
-                    <a class="nav-link active" href="#"><b-badge pill variant="primary">Primary</b-badge></a>
-                    <a class="nav-link active" href="#"><b-badge pill variant="primary">Primary</b-badge></a>
+                    <a class="nav-link text-dark active" href="#">
+                        <i class="ti ti-heart"></i>
+                    </a>
+                    <a class="nav-link text-dark" href="#">
+                        <ti class="ti ti-timer"></ti>
+                    </a>
+                    <a class="nav-link text-dark" href="#">
+                        <ti class="ti ti-list"></ti>
+                    </a>
+                    <a class="nav-link text-dark" href="#">
+                        <ti class="ti ti-save"></ti>
+                    </a>
                 </nav>
             </div>
         </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,36 @@
 <template>
-    <main class="container">
-        <div>Home</div>
+    <main class="container my-4">
+        <div class="page">
+            <h5>djngobat</h5>
+            <div class="row">
+                <div class="col-md-6 col-lg-4">
+                    <b-card title="Card Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article" style="max-width: 20rem;" class="mb-2">
+                        <b-card-text>
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </b-card-text>
+                        <b-button href="#" variant="primary">Go somewhere</b-button>
+                    </b-card>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <b-card title="Card Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article" style="max-width: 20rem;" class="mb-2">
+                        <b-card-text>
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </b-card-text>
+                        <b-button href="#" variant="primary">Go somewhere</b-button>
+                    </b-card>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <b-card title="Card Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article" style="max-width: 20rem;" class="mb-2">
+                        <b-card-text>
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </b-card-text>
+                        <b-button href="#" variant="primary">Go somewhere</b-button>
+                    </b-card>
+                </div> 
+            </div>
+        </div>
     </main>
 </template>
-
 <script>
 export default {
     name: 'Home',
