0c035d6a5de4c1bcd0850971e66de3f0391ba6df
batTrung <trungbat.pv@gmail.com>
2020-07-18 01:04:04 +0700

    custom navbar

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -6,14 +6,33 @@
                 <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
                 <b-collapse id="nav-collapse" is-nav>
                     <b-navbar-nav>
-                        <b-nav-item :to="{name: 'home'}">Trang</b-nav-item>
+                        <b-nav-item>Bài viết</b-nav-item>
+                        <b-nav-item>Danh sách trang</b-nav-item>
                     </b-navbar-nav>
                     <b-navbar-nav class="ml-auto">
                         <b-nav-form>
-                            <b-form-input class="mr-sm-2" placeholder="Search"></b-form-input>
-                            <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Search</b-button>
+                            <div class="input-group bg-white rounded-pill mr-2">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text bg-transparent border-0">
+                                        <i class="ti ti-search"></i>
+                                    </span>
+                                </div>
+                                <input type="text" class="form-control border-0 pb-2 mr-4 pl-0" placeholder="Tìm kiếm bài viết" required="">
+                                <b-nav-item-dropdown right>
+                                    <template slot="button-content">
+                                        <i class="ti ti-world mr-1"></i>
+                                    </template>
+                                    <b-dropdown-item>Tiếng Việt</b-dropdown-item>
+                                    <b-dropdown-item>Tiếng Anh</b-dropdown-item>
+                                </b-nav-item-dropdown>
+                            </div>
                         </b-nav-form>
-                        <b-nav-item-dropdown text="trungbat" right>
+                        <b-nav-item-dropdown right>
+                            <template slot="button-content">
+                                <a href="#" class="icon icon-shape icon-shape-dark rounded-circle">
+                                    <i class="ti ti-user"></i>
+                                </a>
+                            </template>
                             <b-dropdown-item>Trang cá nhân</b-dropdown-item>
                             <b-dropdown-item>Cài đặt</b-dropdown-item>
                             <div class="dropdown-divider"></div>
@@ -25,26 +44,27 @@
         </b-navbar>
         <div class="nav-scroller bg-white box-shadow">
             <div class="container">
-                <nav class="nav nav-underline">
-                    <a
-                        class="nav-link text-dark mb-0 h5"
-                        href="#"
-                        v-tooltip="'Bài viết xem sau'">
-                        <i class="ti ti-timer"></i>
-                    </a>
-                    <a
-                        class="nav-link text-dark mb-0 h5"
-                        href="#"
-                        v-tooltip="'Danh sách đã lưu'">
-                        <i class="ti ti-control-play"></i>
-                    </a>
-                    <a
-                        class="nav-link text-dark mb-0 h5"
-                        href="#"
-                        v-tooltip="'Bài viết đã lưu'">
-                        <i class="ti ti-save"></i>
-                    </a>
-                </nav>
+                <div class="d-flex justify-content-between">
+                    <nav class="nav nav-underline">
+                        <div class="nav-link text-dark mb-0">
+                            Go to:
+                        </div>
+                        <a class="nav-link text-dark mb-0 h5" href="#" v-tooltip="'Bài viết xem sau'">
+                            <i class="ti ti-timer"></i>
+                        </a>
+                        <a class="nav-link text-dark mb-0 h5" href="#" v-tooltip="'Danh sách đã lưu'">
+                            <i class="ti ti-control-play"></i>
+                        </a>
+                        <a class="nav-link text-dark mb-0 h5" href="#" v-tooltip="'Bài viết đã lưu'">
+                            <i class="ti ti-save"></i>
+                        </a>
+                    </nav>
+                    <div class="mt-1">
+                        <a href="#" class="icon icon-shape icon-shape-success rounded-circle">
+                            <i class="ti ti-plus"></i>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
     <main class="container my-4">
         <div class="page">
-            <h5 class="active">Dev.to</h5>
+            <ul class="list-inline">
+                <li class="list-inline-item mr-3">
+                    <a href="" class="h5">Dev.to</a>
+                </li>
+                <li class="list-inline-item mr-3 active">
+                    <a href="" class="active">Mới nhất</a>
+                </li>
+                <li class="list-inline-item mr-3">
+                    <a href="">Xem nhiều</a>
+                </li>
+            </ul>
             <div class="row">
                 <div class="col-lg-3 col-md-4 col-sm-6 my-2">
                     <div class="card h-100 d-flex flex-column justify-content-between shadow-sm">
@@ -10,9 +20,40 @@
                             <span class="badge badge-secondary position-absolute listing-badge"></span>
                         </a>
                         <div class="card-body">
+                            <div class="post-meta mb-2 small text-gray">
+                                <span class="mr-3"><i class="ti ti-eye text-dark mr-2"></i>1</span>
+                                <span><i class="ti ti-timer text-primary mr-2"></i>31</span>
+                            </div>
+                            <a href="/courses/authentication-with-django-react-and-docker-19/">
+                                <div class="mb-2 h6 font-weight-normal">Authentication with Django, React, and Docker-19</div>
+                            </a>
+                            <div class="post-meta">
+                                <div class="media mr-2">
+                                    <div class="avatar-sm mr-1">
+                                        <a class="text-center" href="">
+                                            <div class="text-white rounded-circle mb-2 bg-primary">
+                                                U
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="avatar-name">
+                                        <a class="text-gray small" href="#">dev.to</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6 my-2">
+                    <div class="card h-100 d-flex flex-column justify-content-between shadow-sm">
+                        <a href="/courses/authentication-with-django-react-and-docker-19/" class="position-relative">
+                            <img src="https://images.pexels.com/photos/34600/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top p-2" alt="Authentication with Django, React, and Docker-19">
+                            <span class="badge badge-secondary position-absolute listing-badge"></span>
+                        </a>
+                        <div class="card-body">
                             <div class="post-meta mb-2 small">
                                 <a class="mr-3"><i class="ti ti-eye text-dark mr-2"></i>1</a>
-                                <a class="mr-3"><i class="ti ti-heart text-danger mr-2"></i>0</a>
+                                
                                 <span class="font-small"><i class="ti ti-timer text-primary mr-2"></i>31</span>
                             </div>
                             <a href="/courses/authentication-with-django-react-and-docker-19/">
@@ -44,7 +85,7 @@
                         <div class="card-body">
                             <div class="post-meta mb-2 small">
                                 <a class="mr-3"><i class="ti ti-eye text-dark mr-2"></i>1</a>
-                                <a class="mr-3"><i class="ti ti-heart text-danger mr-2"></i>0</a>
+                                
                                 <span class="font-small"><i class="ti ti-timer text-primary mr-2"></i>31</span>
                             </div>
                             <a href="/courses/authentication-with-django-react-and-docker-19/">
@@ -76,7 +117,7 @@
                         <div class="card-body">
                             <div class="post-meta mb-2 small">
                                 <a class="mr-3"><i class="ti ti-eye text-dark mr-2"></i>1</a>
-                                <a class="mr-3"><i class="ti ti-heart text-danger mr-2"></i>0</a>
+                                
                                 <span class="font-small"><i class="ti ti-timer text-primary mr-2"></i>31</span>
                             </div>
                             <a href="/courses/authentication-with-django-react-and-docker-19/">
@@ -99,38 +140,6 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-4 col-sm-6 my-2">
-                    <div class="card h-100 d-flex flex-column justify-content-between shadow-sm">
-                        <a href="/courses/authentication-with-django-react-and-docker-19/" class="position-relative">
-                            <img src="https://images.pexels.com/photos/34600/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top p-2" alt="Authentication with Django, React, and Docker-19">
-                            <span class="badge badge-secondary position-absolute listing-badge"></span>
-                        </a>
-                        <div class="card-body">
-                            <div class="post-meta mb-2 small">
-                                <a class="mr-3"><i class="ti ti-eye text-dark mr-2"></i>1</a>
-                                <a class="mr-3"><i class="ti ti-heart text-danger mr-2"></i>0</a>
-                                <span class="font-small"><i class="ti ti-timer text-primary mr-2"></i>31</span>
-                            </div>
-                            <a href="/courses/authentication-with-django-react-and-docker-19/">
-                                <div class="mb-2 h6 font-weight-normal">Authentication with Django, React, and Docker-19</div>
-                            </a>
-                            <div class="post-meta">
-                                <div class="media mr-2">
-                                    <div class="avatar-sm mr-1">
-                                        <a class="text-center" href="">
-                                            <div class="text-white rounded-circle mb-2 bg-primary">
-                                                U
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <div class="avatar-name">
-                                        <a class="text-gray small" href="#">dev.to</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>   
             </div>
         </div>
     </main>
