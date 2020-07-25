70701fa746d89b4859fd3614aa8de792945b4c3c
batTrung <trungbat.pv@gmail.com>
2020-07-25 21:21:08 +0700

    Change static folder to dist

--- backend/settings.py
+++ backend/settings.py
@@ -174,7 +174,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'dist', 'static'),
+    os.path.join(BASE_DIR, 'dist'),
 ]
 
 MEDIA_URL = '/media/'
