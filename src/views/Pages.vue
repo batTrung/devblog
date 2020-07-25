6fb78dac5721f38ccb36fc871f085a97338043bc
batTrung <trungbat.pv@gmail.com>
2020-07-25 23:47:36 +0700

    Log API_URL

--- src/views/Pages.vue
+++ src/views/Pages.vue
@@ -101,6 +101,7 @@ export default {
         },
     },
     mounted() {
+        console.log('API_URL: ', process.env.VUE_APP_API_URL)
         this.$store.dispatch(FETCH_TOPICS, { website: true })
     },
 }
