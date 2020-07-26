321aed8600455de13f0a0f6e608d71ed43e287be
batTrung <trungbat.pv@gmail.com>
2020-07-26 18:47:35 +0700

    Add more wake_up urls

--- apps/blog/management/commands/run_crawler.py
+++ apps/blog/management/commands/run_crawler.py
@@ -9,5 +9,11 @@ from apps.blog.crawler import blog_crawler
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        blog_crawler.run()
+        requests.get('https://videv-blog.herokuapp.com/api/v1/posts/')
+        requests.get('https://videv-blog.herokuapp.com/api/v1/websites/')
+        requests.get('https://videv-blog.herokuapp.com/api/v1/playlists/')
+        requests.get('https://videv-blog.herokuapp.com/api/v1/topics/')
         requests.get('https://videv-blog.herokuapp.com/')
+        requests.get('https://videv-blog.herokuapp.com/posts/')
+        requests.get('https://videv-blog.herokuapp.com/pages/')
+        blog_crawler.run()
